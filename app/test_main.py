b4585d3162345faef5c4cd8e51362eb1935e677f
unknown <bku089@gmail.com>
2024-12-22 20:56:48 +0200

    done, but not finish

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,6 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_basic_case() -> None:
+    result = get_coin_combination(1)
+    assert result == [1, 0, 0, 0], "It was expected [1, 0, 0, 0] for 1 cent"
