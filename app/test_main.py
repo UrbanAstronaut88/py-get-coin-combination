2bd336274f230e8263618c2dcf3ad59bed00079f
unknown <bku089@gmail.com>
2025-02-02 21:26:30 +0200

    DONE

--- app/test_main.py
+++ app/test_main.py
@@ -4,3 +4,6 @@ from app.main import get_coin_combination
 def test_basic_case() -> None:
     result = get_coin_combination(1)
     assert result == [1, 0, 0, 0], "It was expected [1, 0, 0, 0] for 1 cent"
+
+def test_combination() -> None:
+    assert app.main.get_coin_combination(41) == [1, 1, 1, 1], "Ошибка для 41 цента"
